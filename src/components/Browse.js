b354ae75d201e05340b6e35f1171e1709d58922c
Neuron7 <rk0004334@gmail.com>
2025-07-10 14:16:28 +0530

    project setup, SignIn/SignUp form and Header

---
+++ src/components/Browse.js
@@ -0,0 +1,9 @@
+const Browse = () =>{
+    return (
+        <>
+        <h1 className="text-red-500">Browse</h1>
+        </>
+    )
+}
+
+export default Browse;
---
+++ src/components/Login.js
@@ -0,0 +1,31 @@
+import Header from "./Header";
+import { useState } from "react";
+const Login = () =>{
+    const [isSignUp, setIsSignUp] = useState(false);
+    const handleToggle = ()=>{
+        setIsSignUp(!isSignUp);
+    }
+    return(
+        <div >
+            <Header/>
+            <div className="absolute">
+                <img src="https://assets.nflxext.com/ffe/siteui/vlv3/7d2359a4-434f-4efa-9ff3-e9d38a8bde7f/web/IN-en-20250707-TRIFECTA-perspective_4faa9280-a2c5-4e07-aafc-a45ce43fea09_large.jpg"
+                alt="backgroundImage"/>
+            </div>
+            <form className="w-3/12 p-12 bg-black absolute my-36 mx-auto right-0 left-0 text-white bg-opacity-75">
+                <h1>{isSignUp ? "Sign Up" : "Sign In"}</h1>
+                {isSignUp && 
+                    <input type="text" placeholder="Email Address" className="p-4 my-4 w-full bg-gray-700"/>
+                }
+                <input type="text" placeholder="Email Address" className="p-4 my-4 w-full bg-gray-700"/>
+                <input type="password" placeholder="Password" className="p-4 my-4 w-full bg-gray-700"/>
+                <button className="p-4 my-6 bg-red-600 w-full rounded-lg" >
+                    {isSignUp ? "Sign Up" : "Sign In"}
+                </button>
+                <p className="py-4 cursor-pointer" onClick={handleToggle}>{!isSignUp ? "New to Netflix? Sign up now" : "Already register, Login now"}</p>
+            </form>
+        </div>
+    );
+}
+
+export default Login;
